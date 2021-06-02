61f5266b8f296c45dccc14edbad7e38986123b43
intancious <intancious98@gmail.com>
2021-06-02 13:30:32 +0700

    dah tak tambah db terbaru

--- coba_pkl.sql
+++ coba_pkl.sql
@@ -1,143 +1,298 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2021 at 08:28 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
- Source Server         : local
- Source Server Type    : MySQL
- Source Server Version : 100121
- Source Host           : localhost:3306
- Source Schema         : coba_pkl
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100121
- File Encoding         : 65001
 
- Date: 02/06/2021 01:31:13
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `coba_pkl`
+--
 
--- ----------------------------
--- Table structure for mhs_tif
--- ----------------------------
-DROP TABLE IF EXISTS `mhs_tif`;
-CREATE TABLE `mhs_tif`  (
-  `Id_mhs_tif` int(11) NOT NULL AUTO_INCREMENT,
-  `nim` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `nama_mahasiswa` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `nama_file` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `foto` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`Id_mhs_tif`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of mhs_tif
--- ----------------------------
-INSERT INTO `mhs_tif` VALUES (12, 'sgs', 'fhfh', '3671_PL17PP_20211.pdf', 'Logo_Garuda.png');
+--
+-- Table structure for table `mhs_mif`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `email` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `image` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+CREATE TABLE `mhs_mif` (
+  `Id_mhs_mif` int(11) NOT NULL,
+  `nim` varchar(255) NOT NULL,
+  `nama_mahasiswa` varchar(255) NOT NULL,
+  `nama_file` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mhs_mif`
+--
+
+INSERT INTO `mhs_mif` (`Id_mhs_mif`, `nim`, `nama_mahasiswa`, `nama_file`, `foto`) VALUES
+(0, 'E31171294', 'Intan Permatasari', 'tutor_mas_hariz.PNG', 'tutor_mas_hariz1.PNG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs_tif`
+--
+
+CREATE TABLE `mhs_tif` (
+  `Id_mhs_tif` int(11) NOT NULL,
+  `nim` varchar(255) DEFAULT NULL,
+  `nama_mahasiswa` varchar(255) DEFAULT NULL,
+  `nama_file` varchar(255) DEFAULT NULL,
+  `foto` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `mhs_tif`
+--
+
+INSERT INTO `mhs_tif` (`Id_mhs_tif`, `nim`, `nama_mahasiswa`, `nama_file`, `foto`) VALUES
+(12, 'sgs', 'fhfh', '3671_PL17PP_20211.pdf', 'Logo_Garuda.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs_tkk`
+--
+
+CREATE TABLE `mhs_tkk` (
+  `Id_mhs_tkk` int(11) NOT NULL,
+  `nim` varchar(255) NOT NULL,
+  `nama_mahasiswa` varchar(255) NOT NULL,
+  `nama_file` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `data_created` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `data_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'asd', 'asd@gmail.com', 'default.jpg', '$2y$10$CCJyNvUPFrDdd.XBI4ODm.SiR.eGYXT/7Rgp034tjH14ZJhz1XYt2', 2, 1, 1603802141);
-INSERT INTO `user` VALUES (2, 'qw', 'qw@yahoo.com', 'default.jpg', '$2y$10$Qjrs1l.HBEonRe9/tZA4kuKgbvaTaA05K.m80PNLkcoPd2GH6crI2', 1, 1, 1603802968);
-INSERT INTO `user` VALUES (3, 'erik', 'erik@gmail.com', 'default.jpg', '$2y$10$uG/DqrKzviwzvYWEoLBpE.bCgv68uofaRn3d9VQ6y5EmbAyBDXlwi', 2, 1, 1605017081);
-INSERT INTO `user` VALUES (4, 'na', 'na@gmail.com', 'default.jpg', '$2y$10$0U4hIvrJMgCcXGkW45UTI.GTTrQD3xDC00VBee4yjtlQe5t4pJZ.2', 2, 0, 1605017152);
+--
+-- Dumping data for table `user`
+--
 
--- ----------------------------
--- Table structure for user_access_menu
--- ----------------------------
-DROP TABLE IF EXISTS `user_access_menu`;
-CREATE TABLE `user_access_menu`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `data_created`) VALUES
+(1, 'asd', 'asd@gmail.com', 'default.jpg', '$2y$10$CCJyNvUPFrDdd.XBI4ODm.SiR.eGYXT/7Rgp034tjH14ZJhz1XYt2', 2, 1, 1603802141),
+(2, 'qw', 'qw@yahoo.com', 'default.jpg', '$2y$10$Qjrs1l.HBEonRe9/tZA4kuKgbvaTaA05K.m80PNLkcoPd2GH6crI2', 1, 1, 1603802968),
+(3, 'erik', 'erik@gmail.com', 'default.jpg', '$2y$10$uG/DqrKzviwzvYWEoLBpE.bCgv68uofaRn3d9VQ6y5EmbAyBDXlwi', 2, 1, 1605017081),
+(4, 'na', 'na@gmail.com', 'default.jpg', '$2y$10$0U4hIvrJMgCcXGkW45UTI.GTTrQD3xDC00VBee4yjtlQe5t4pJZ.2', 2, 0, 1605017152);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2),
+(5, 2, 3),
+(6, 1, 3),
+(8, 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(126) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'admin'),
+(2, 'user'),
+(3, 'Menu'),
+(7, 'Teknologi Informasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'Member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
   `menu_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of user_access_menu
--- ----------------------------
-INSERT INTO `user_access_menu` VALUES (1, 1, 1);
-INSERT INTO `user_access_menu` VALUES (2, 1, 2);
-INSERT INTO `user_access_menu` VALUES (3, 2, 2);
-INSERT INTO `user_access_menu` VALUES (5, 2, 3);
-INSERT INTO `user_access_menu` VALUES (6, 1, 3);
-INSERT INTO `user_access_menu` VALUES (8, 1, 7);
+--
+-- Dumping data for table `user_sub_menu`
+--
 
--- ----------------------------
--- Table structure for user_menu
--- ----------------------------
-DROP TABLE IF EXISTS `user_menu`;
-CREATE TABLE `user_menu`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `menu` varchar(126) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(4, 3, 'Menu Management', 'menu', 'fas  fa-fw fa-folder', 1),
+(5, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(7, 1, 'coba', 'menu/coba', 'fas fa-fw fw-folder', 1),
+(9, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
+(10, 7, 'Teknik Informatika', 'TIF', 'fas fa-fw fa-microchip', 1),
+(12, 7, 'Manajemen Informatika', 'MIF', 'fas fa-laptop-code', 1),
+(13, 7, 'Teknik Komputer', 'TKK', 'fas fa-cogs', 1);
 
--- ----------------------------
--- Records of user_menu
--- ----------------------------
-INSERT INTO `user_menu` VALUES (1, 'admin');
-INSERT INTO `user_menu` VALUES (2, 'user');
-INSERT INTO `user_menu` VALUES (3, 'Menu');
-INSERT INTO `user_menu` VALUES (7, 'Teknologi Informasi');
+--
+-- Indexes for dumped tables
+--
 
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+--
+-- Indexes for table `mhs_mif`
+--
+ALTER TABLE `mhs_mif`
+  ADD PRIMARY KEY (`Id_mhs_mif`);
 
--- ----------------------------
--- Records of user_role
--- ----------------------------
-INSERT INTO `user_role` VALUES (1, 'Administrator');
-INSERT INTO `user_role` VALUES (2, 'Member');
+--
+-- Indexes for table `mhs_tif`
+--
+ALTER TABLE `mhs_tif`
+  ADD PRIMARY KEY (`Id_mhs_tif`) USING BTREE;
 
--- ----------------------------
--- Table structure for user_sub_menu
--- ----------------------------
-DROP TABLE IF EXISTS `user_sub_menu`;
-CREATE TABLE `user_sub_menu`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `menu_id` int(11) NOT NULL,
-  `title` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `url` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `icon` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `is_active` int(1) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+--
+-- Indexes for table `mhs_tkk`
+--
+ALTER TABLE `mhs_tkk`
+  ADD PRIMARY KEY (`Id_mhs_tkk`);
 
--- ----------------------------
--- Records of user_sub_menu
--- ----------------------------
-INSERT INTO `user_sub_menu` VALUES (1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1);
-INSERT INTO `user_sub_menu` VALUES (2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1);
-INSERT INTO `user_sub_menu` VALUES (3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1);
-INSERT INTO `user_sub_menu` VALUES (4, 3, 'Menu Management', 'menu', 'fas  fa-fw fa-folder', 1);
-INSERT INTO `user_sub_menu` VALUES (5, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1);
-INSERT INTO `user_sub_menu` VALUES (7, 1, 'coba', 'menu/coba', 'fas fa-fw fw-folder', 0);
-INSERT INTO `user_sub_menu` VALUES (9, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1);
-INSERT INTO `user_sub_menu` VALUES (10, 7, 'Teknik Informatika', 'TIF', 'fas fa-fw fa-microchip', 1);
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mhs_tif`
+--
+ALTER TABLE `mhs_tif`
+  MODIFY `Id_mhs_tif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
