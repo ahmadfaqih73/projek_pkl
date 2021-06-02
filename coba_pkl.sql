be116dc319b0363402080a7072a2d70cecf46087
intancious <intancious98@gmail.com>
2021-06-02 13:44:34 +0700

    database terbaru

--- coba_pkl.sql
+++ coba_pkl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 08:28 AM
+-- Generation Time: Jun 02, 2021 at 08:42 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
