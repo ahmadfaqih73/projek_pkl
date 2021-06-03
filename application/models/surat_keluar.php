f5d9d24b79a5eea7dcf3a49f9749d5bcd0f9b811
ahmadfaqih73 <40154392+ahmadfaqih73@users.noreply.github.com>
2021-06-03 14:54:17 +0700

    con troller surat keluar,model error

--- application/models/surat_keluar.php
+++ application/models/surat_keluar.php
@@ -1,11 +1,14 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
+ <?php
+    defined('BASEPATH') or exit('No direct script access allowed');
 
-class Surat_keluar extends CI_Model
-{
-    public function read()
+    class Surat_Keluar extends CI_Model
     {
-        $q_read = $this->db->get('surat_keluar')->result_array();
-        return $q_read;
+
+
+
+        public function read()
+        {
+            $q_read = $this->db->get('surat_keluar')->result_array();
+            return $q_read;
+        }
     }
-}
