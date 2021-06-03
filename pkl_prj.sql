60051cd34f538d5c4ff57ba7d29a7c5708a8bac7
ahmadfaqih73 <40154392+ahmadfaqih73@users.noreply.github.com>
2021-06-03 13:03:12 +0700

    database

--- pkl_prj.sql
+++ pkl_prj.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.16  Distrib 10.1.36-MariaDB, for Win32 (AMD64)
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
 --
--- Host: localhost    Database: coba_pkl
+-- Host: localhost    Database: localhost
 -- ------------------------------------------------------
--- Server version	10.1.36-MariaDB
+-- Server version	10.1.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jenis_surat`
+--
+
+DROP TABLE IF EXISTS `jenis_surat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jenis_surat` (
+  `id_jenis surat` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis_surat` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_jenis surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jenis_surat`
+--
+
+LOCK TABLES `jenis_surat` WRITE;
+/*!40000 ALTER TABLE `jenis_surat` DISABLE KEYS */;
+INSERT INTO `jenis_surat` VALUES (1,'surat undangan'),(2,'surat Akademi');
+/*!40000 ALTER TABLE `jenis_surat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `kategori_sm`
@@ -116,6 +140,32 @@ CREATE TABLE `mhs_tkk` (
 LOCK TABLES `mhs_tkk` WRITE;
 /*!40000 ALTER TABLE `mhs_tkk` DISABLE KEYS */;
 /*!40000 ALTER TABLE `mhs_tkk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `surat_keluar`
+--
+
+DROP TABLE IF EXISTS `surat_keluar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `surat_keluar` (
+  `id_surat` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis_surat` int(11) DEFAULT NULL,
+  `file_surat` varchar(255) DEFAULT NULL,
+  `deskripsi_surat` varchar(255) DEFAULT NULL,
+  `tanggal_surat_keluar` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `surat_keluar`
+--
+
+LOCK TABLES `surat_keluar` WRITE;
+/*!40000 ALTER TABLE `surat_keluar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `surat_keluar` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -284,4 +334,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-02 20:55:13
+-- Dump completed on 2021-06-03 12:59:55
